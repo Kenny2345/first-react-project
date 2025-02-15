fa222e934b663970b103bb7281a04f4c04f18676
Kenny <adebari@gmail.com>
2025-02-15 23:48:18 +0100

    Co-authored-by: Tobiloba Osoba <tobiosoba777@gmail.com>

--- src/TicketConfirmation.jsx
+++ src/TicketConfirmation.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import barcodeImg from '../barcode.avif'
+// import barcodeImg from '../barcode.avif'
 import './App.css'
 
 function TicketConfirmation() {
